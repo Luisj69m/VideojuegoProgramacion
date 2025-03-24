ebd0e94254ed0ce75598c57f8e3f03e39c82996a
Luisj69m <luisjosemarcanofuentes@gmail.com>
2025-03-24 23:54:12 +0100

    Parte de Luis 1

---
+++ VideojuegoProgramacion.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class VideojuegoProgramacion {
+      public static void main(String[] args) {
+        String configFilePath = "configuracion.cfg";
+        String[] directorios = {"escenarios", "jugadores", "partidas"};
+
+        File configFile = new File(configFilePath);
+
+    }
+}
