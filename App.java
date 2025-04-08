aed5bd8ed0d4bfbaf696173bffd93a4736b64ce3
armyvan <rubiomurilloivan@gmail.com>
2025-04-08 12:48:08 +0200

    parte 4 Ivan

--- App.java
+++ App.java
@@ -3,32 +3,16 @@ import controlador.Controlador;
 import modelo.Configuracion;
 import modelo.Jugador;
 
-/**
- * Clase principal de la aplicación.
- * Se encarga de inicializar la configuración, crear el controlador y gestionar el jugador.
- * @author Luis José Marcano
- * @author Dani Moñino
- * @author Ivan Rubio
- * Licencia: GPL V3.0
-
- */
 public class App {
-    /**
-     * Método principal que inicia la ejecución del programa.
-     * 
-     * @param args Argumentos de línea de comandos (no utilizados).
-     */
     public static void main(String[] args) {
-        // Inicialización de la configuración (fichero de configuración y directorios)
         Configuracion.inicializar();
 
-        // Crear el controlador con el archivo de escenario especificado
         Controlador controlador = new Controlador("escenarios/escenario1.txt");
 
-        // Iniciar jugador (cargar o crear jugador)
-        Jugador jugador = controlador.iniciarJugador();  // Se llama al método desde el controlador
+        Jugador jugador = controlador.iniciarJugador();
 
-        // Mostrar el escenario
         controlador.mostrarEscenario();
-    }
+
+        controlador.iniciarJuegoInteractivo();
+    }
 }
